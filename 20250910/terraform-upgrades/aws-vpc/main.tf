0da6738eb7e71e78fff7b3c07bc8fdb2b48a6d01
Leonardo Taccari <iamleot@gmail.com>
2025-09-10 23:11:03 +0200

    Add a simple example to create an AWS VPC
    Similar to the S3 bucket example, this is a bit more complicated and
    creates more resources.
    
    terraform-aws-modules/vpc/aws is pinned to 3.7.0 because it was the
    last version to work with Terraform 0.12.x.

---
+++ 20250910/terraform-upgrades/aws-vpc/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    ec2 = "http://localhost:5000"
+    iam = "http://localhost:5000"
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.7.0"
+
+  name = "testing-aws-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_dns_hostnames = true
+  enable_nat_gateway   = true
+
+  tags = {
+    Terraform = "true"
+  }
+}
